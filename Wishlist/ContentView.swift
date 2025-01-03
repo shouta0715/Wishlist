ec721249fc56f9804867760a4b6b50c82d54c2bb
shouta0715 <Myheroes0715@gmail.com>
2025-01-03 21:41:15 +0900

    Initial Commit

---
+++ Wishlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wishlist
+//
+//  Created by k21047kk on 2025/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by k21047kk on 2025/01/03.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
